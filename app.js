81e229d0601ddc011f71dde8878e7a0ce39d94cc
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-30 13:14:55 +0300

    - function drawBoard

--- app.js
+++ app.js
@@ -6,14 +6,30 @@ const COL = COLUMN = 10;
 const SQ = squareSize = 20;
 const VACANT = 'WHITE';
 
-function draw(x, y, color) {
-    ctx.fillStyle = 'red';
+function drawSquare(x, y, color) {
+    ctx.fillStyle = color;
     ctx.fillRect( x*SQ,y*SQ,SQ,SQ );
 
     ctx.strokeStyle = 'BLACK';
     ctx.strokeRect( x*SQ,y*SQ,SQ,SQ );
 }
 
-draw(0, 0, 'red');
 
+let board = [];
 
+for ( r = 0; r < ROW; r++ ) {
+    board[r] = [];
+    for (c = 0; c < COL; c++) {
+        board[r][c] = VACANT;
+    }
+}
+
+function drawBoard() {
+    for ( r = 0; r < ROW; r++ ) {
+        for (c = 0; c < COL; c++) {
+                drawSquare(c, r, board[r][c]);
+            }
+        }
+    }
+
+    drawBoard();
