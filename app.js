328e643070817840ce3665d1c4b6643c12737bc0
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-31 22:51:56 +0300

    - tetrominoes.js

--- app.js
+++ app.js
@@ -17,31 +17,31 @@ function drawSquare(x, y, color) {
 
 let board = [];
 
-for ( r = 0; r < ROW; r++ ) {
+for( r = 0; r <ROW; r++){
     board[r] = [];
-    for (c = 0; c < COL; c++) {
+    for(c = 0; c < COL; c++){
         board[r][c] = VACANT;
     }
 }
 
-function drawBoard() {
-    for ( r = 0; r < ROW; r++ ) {
-        for (c = 0; c < COL; c++) {
-                drawSquare(c, r, board[r][c]);
-            }
+function drawBoard(){
+    for( r = 0; r <ROW; r++){
+        for(c = 0; c < COL; c++){
+            drawSquare(c,r,board[r][c]);
         }
     }
+}
 
-    drawBoard();
+drawBoard();
 
 const PIECES = [
-    [Z, 'red'],
-    [S, 'green'],
-    [T, 'yellow'],
-    [O, 'blue'],
-    [L, 'purple'],
-    [I, 'cyan'],
-    [J, 'orange']
+    [Z, "red"],
+    [S, "green"],
+    [T, "yellow"],
+    [O, "blue"],
+    [L, "purple"],
+    [I, "cyan"],
+    [J, "orange"]
 ];
 
 let p = new Piece( PIECES[0][0], PIECES[0][1] );
@@ -54,6 +54,18 @@ function Piece(tetromino, color) {
     this.tetrominoN = 0;
     this.activeTetromino = this.tetromino[this.tetrominoN];
 
-    this.x = 0;
-    this.y = 0;
+    this.x = 3;
+    this.y = 5;
 }
+
+Piece.prototype.draw = function () {
+    for (r = 0; r < this.activeTetromino.length; r++) {
+            for (c = 0; c < this.activeTetromino.length; c++) {
+               if (this.activeTetromino[r][c]) {
+                   drawSquare(this.x + c, this.y + r, this.color)
+               }
+            }
+        }
+    };
+
+p.draw();
